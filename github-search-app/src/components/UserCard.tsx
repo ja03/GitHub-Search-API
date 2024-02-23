b278040c38f23b5eb4e159f8277eeb22d7ae0794
Ahmad Mahmoud <abuhamdiehahmad@gmail.com>
2024-02-23 23:28:45 +0300

    fixed handleFavUser and handleStarIcon functions

--- github-search-app/src/components/UserCard.tsx
+++ github-search-app/src/components/UserCard.tsx
@@ -7,7 +7,7 @@ import { handleFavUser, handleStarIcon } from "../utils/helper";
 import starIcon from "../assets/icons/StarBorderOutlined.png";
 import filledStarIcon from "../assets/icons/StarFilled.png";
 
-const UserCard = ({userData}:UserCardProps) => {
+const UserCard = ({ userData }: UserCardProps) => {
     const [isFav, setisFav]= useState<boolean>(false)
     useEffect(()=>{
         let tempState:boolean = handleStarIcon(userData)
@@ -18,15 +18,24 @@ const UserCard = ({userData}:UserCardProps) => {
             <div className="flex-1 flex items-center gap-2">
                 <div
                     className="rounded-full border-2 w-[60px] h-[60px] grid items-center  bg-cover bg-center"
-                    style={{ backgroundImage: `url(${userData.user_avatar})` }}>
-                </div>
+                    style={{
+                        backgroundImage: `url(${userData.user_avatar})`,
+                    }}></div>
                 <p className="text-[18px]">@{userData.user_login}</p>
             </div>
             {isFav ? (
-                <img src={filledStarIcon}  alt="filled-star-icon" onClick={()=>handleFavUser(userData)}/>
+                <img
+                    src={filledStarIcon}
+                    alt="filled-star-icon"
+                    onClick={() => handleFavUser(userData)}
+                />
             ) : (
                 <>
-                    <img src={starIcon}  alt="star-icon" onClick={()=>handleFavUser(userData)}/>
+                    <img
+                        src={starIcon}
+                        alt="star-icon"
+                        onClick={() => handleFavUser(userData)}
+                    />
                 </>
             )}
         </div>
--- github-search-app/src/utils/helper.ts
+++ github-search-app/src/utils/helper.ts
@@ -2,38 +2,37 @@ import { User } from "./types";
 export const handleFavUser = (user: User): void => {
     // chechk if user in array
     let favUsersItem: string = localStorage.getItem("FavUsers") ?? "";
-    let inArray:boolean;
-    let favUsers: object[] 
-    if(favUsersItem.length > 0){
+    let inArray: boolean;
+    let favUsers: User[];
+    if (favUsersItem.length > 0) {
         favUsers = JSON.parse(favUsersItem);
-        inArray = favUsers.includes(user);
-    }else{
-        inArray=false
-        favUsers=[]
+        inArray = handleStarIcon(user);
+    } else {
+        inArray = false;
+        favUsers = [];
     }
     if (inArray) {
         // pop item
+        console.log("removing user")
         let newArr = favUsers.filter((u) => {
-            return u !== user;
+            return u.user_login !== user.user_login;
         });
-        localStorage.setItem("favUsersItem", JSON.stringify(newArr));
+        localStorage.setItem("favUsers", JSON.stringify(newArr));
     } else {
         // add item
+        console.log("adding user")
         favUsers.push(user);
         localStorage.setItem("FavUsers", JSON.stringify(favUsers));
     }
 };
 export const handleStarIcon = (user: User): boolean => {
     let favUsersItem: string = localStorage.getItem("FavUsers") ?? "";
-    if (favUsersItem.length > 0){
-        let favUsers: object[] = JSON.parse(favUsersItem);
-        if(favUsers.includes(user)){
-            return true
-        }else{
-            return false
-        }
-    }else{
-        return false
+    if (favUsersItem.length > 0) {
+        let favUsers: User[] = JSON.parse(favUsersItem);
+        return favUsers.some((u) => {
+            return u.user_login === user.user_login;
+        });
+    } else {
+        return false;
     }
-    
 };
