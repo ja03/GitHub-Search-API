6cca28f4c2432d1a9721522185c199d326b1b999
Ahmad Mahmoud <abuhamdiehahmad@gmail.com>
2024-02-23 20:50:49 +0300

    added handleStarIcon function

--- github-search-app/src/utils/helper.ts
+++ github-search-app/src/utils/helper.ts
@@ -1,24 +1,24 @@
-import { User } from "./types"
-export const handleFavUser = (user:User, users:User[]=[]):void =>{
+import { User } from "./types";
+export const handleFavUser = (user: User, users: User[] = []): void => {
     // chechk if user in array
-    let favUsersItem : string = localStorage.getItem('FavUsers') ?? " "
-    let favUsers:(object)[] = JSON.parse(favUsersItem)
-    let inArray = favUsers.includes(user)
-    if(inArray){
-        // pop item 
-        let newArr = favUsers.filter(u =>{
-            return (u !== user)
-        })
-        localStorage.setItem('favUsersItem', JSON.stringify(newArr))
-    }else{
-        // add item 
-        users.push(user)
-        localStorage.setItem('FavUsers', JSON.stringify(users))
+    let favUsersItem: string = localStorage.getItem("FavUsers") ?? " ";
+    let favUsers: object[] = JSON.parse(favUsersItem);
+    let inArray = favUsers.includes(user);
+    if (inArray) {
+        // pop item
+        let newArr = favUsers.filter((u) => {
+            return u !== user;
+        });
+        localStorage.setItem("favUsersItem", JSON.stringify(newArr));
+    } else {
+        // add item
+        users.push(user);
+        localStorage.setItem("FavUsers", JSON.stringify(users));
     }
-    localStorage.setItem('users', JSON.stringify(user))
-}
-export const handleStarIcon= (user:User):boolean=>{
-    let favUsersItem : string = localStorage.getItem('FavUsers') ?? " "
-    let favUsers:(object)[] = JSON.parse(favUsersItem)
-    return favUsers.includes(user)
-}
+    localStorage.setItem("users", JSON.stringify(user));
+};
+export const handleStarIcon = (user: User): boolean => {
+    let favUsersItem: string = localStorage.getItem("FavUsers") ?? " ";
+    let favUsers: object[] = JSON.parse(favUsersItem);
+    return favUsers.includes(user);
+};
